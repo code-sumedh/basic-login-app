a1b0ae2c4fa403f977eb2d7a55641a73bbf25be3
nagajyothi1722 <nagajyothi1722@gmail.com>
2020-09-08 19:09:24 +0530

    Changes to carousel and password

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.css';
-import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
-import image1 from './Images/1.jpg'
-import image2 from './Images/2.jpg'
-import image3 from './Images/3.jpg'
+import Carousel from './Carousel';
 
 
 function App() {
@@ -29,7 +25,7 @@ function App() {
            <input type="text" id="email" name="email" placeholder="josh@gmail.com" onChange={changeHandler}/>
 
            <label for="lname">Password</label>
-           <input type="text" id="password" name="password" placeholder="******" />
+           <input type="password" id="password" name="password" placeholder="******" />
 
   
            <input type="submit" value="Sign Up"  onClick={loginHandler}/>
@@ -40,11 +36,7 @@ function App() {
        </div>
 
       <div className="split right">
-        <AliceCarousel autoPlay autoPlayInterval="3000">
-         <img src={image1} className="sliderimg"/>
-         <img src={image2} className="sliderimg"/>
-         <img src={image3} className="sliderimg"/>
-        </AliceCarousel>
+      <Carousel/>
       </div>
     </div>
   );
